72f158e88e04a1a4e705adbd901dbededf26cfa7
purushothaman-source <purushoth99jun@gmail.com>
2020-11-23 11:36:40 +0530

    ADDED :password rule4

--- userregistration.sh
+++ userregistration.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 shopt -s extglob
-passPat="^(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]{8,}$"
+passPat="^(?=.*[A-Z])(?=.*[0-9])(?=.*[a-z])(?=.*[,.<>?;:'"!@#$%*-])[a-zA-Z0-9,.?><:"';!@#$%&*]{8,}$"
 read -p "enter valid password " password
 
 if [[ $password =~ $passPat ]]
