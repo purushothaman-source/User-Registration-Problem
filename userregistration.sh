5ae65aa45f6b76c99075cc848c1b548126bd3b0b
purushothaman-source <72643364+purushothaman-source@users.noreply.github.com>
2020-11-30 10:26:53 +0530

    "REFRACTORED : refracted to function"

--- userregistration.sh
+++ userregistration.sh
@@ -1,5 +1,18 @@
 #!/bin/bash 
 shopt -s extglob
+function firstName{
+firstNamePat="^[A-Z]{1}[a-z]{2,}$"
+read -p "enter firstname " firstName
+
+if [[ $firstName =~ $firstNamePat ]]
+then
+   echo "Pattern matching"
+else
+  echo "pattern not matching"
+fi
+}
+firstName
+function lastName{
 lastNamePat="^[A-Z]{1}[a-z]{2,}$"
 read -p "enter lastname " lastName
 
@@ -9,3 +22,4 @@ then
 else
   echo "pattern not matching"
 fi
+}
