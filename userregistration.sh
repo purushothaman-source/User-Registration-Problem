43a397acb0a386d4e444e03095bbe3bb72a0b48d
purushothaman-source <72643364+purushothaman-source@users.noreply.github.com>
2020-11-30 10:29:28 +0530

    REFRACTORED : refract to function

--- userregistration.sh
+++ userregistration.sh
@@ -1,5 +1,31 @@
 #!/bin/bash 
 shopt -s extglob
+function firstName{
+firstNamePat="^[A-Z]{1}[a-z]{2,}$"
+read -p "enter firstname " firstName
+
+if [[ $firstName =~ $firstNamePat ]]
+then
+   echo "Pattern matching"
+else
+  echo "pattern not matching"
+fi
+}
+firstname
+function lastName{
+lastNamePat="^[A-Z]{1}[a-z]{2,}$"
+read -p "enter lastname " lastName
+
+if [[ $lastName =~ $lastNamePat ]]
+then
+   echo "Pattern matching"
+else
+  echo "pattern not matching"
+fi
+}
+lastName
+
+function email{
 emailPat="^[0-9a-zA-Z]+([.\-_+][0-9a-zA-Z]+)*@[a-z0-9A-Z]+.[a-z]{2,4}([.][a-zA-Z]{2})*$"
 read -p "enter valid email " email
 
@@ -9,3 +35,5 @@ then
 else
   echo "pattern not matching"
 fi
+}
+email
